f47c5f912f672f0a6f9ac079c71fa6152208785c
ebinxavier <ebinx7@gmail.com>
2019-11-28 18:06:14 +0400

    added args in pupeteer

--- server.js
+++ server.js
@@ -3,6 +3,17 @@ const jsreport = require('jsreport')({
   httpPort: process.env.PORT || 3456
 })
 
+jsreport.use(require('jsreport-chrome-pdf')(
+  {
+    launchOptions: {
+      args: [
+        '--no-sandbox',
+        '--disable-setuid-sandbox',
+      ]
+    }
+  }
+  ));
+
 if (process.env.JSREPORT_CLI) {
   // export jsreport instance to make it possible to use jsreport-cli
   module.exports = jsreport
